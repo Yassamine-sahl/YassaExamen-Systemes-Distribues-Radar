1c12f381b2de2ac1ae1b84eede6263b3e131db65
Yassamine-sahl <yassamine.sahl@gmail.com>
2022-12-25 13:25:00 +0100

    Common-api

---
+++ common-api/src/main/java/com/example/commonapi/queries/GetOwner.java
@@ -0,0 +1,11 @@
+package com.example.commonapi.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class GetOwner {
+    public String id;
+}
---
+++ common-api/src/main/java/com/example/commonapi/queries/GetVehicule.java
@@ -0,0 +1,13 @@
+package com.example.commonapi.queries;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetVehicule {
+    private String id;
+}
